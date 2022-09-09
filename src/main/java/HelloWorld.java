23ff5b2f5206e1ad05093f08abf6d76aec0d98d9
Nia Watson <nia.d.watson@gmail.com>
2022-09-09 09:54:19 -0500

    feat. Into to Java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello ");
+        System.out.print("World");
+        System.out.println("Nia Watson");
+        System.out.print("Nia ");
+        System.out.print("Watson");
+    }
+}
+
