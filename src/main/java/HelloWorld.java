1b50b2c07b271667e4ddb4db1cc90299776c1559
Nia Watson <nia.d.watson@gmail.com>
2022-09-09 16:42:41 -0500

    feat. increments

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,31 @@
 import java.util.Arrays;
+import java.util.function.ToDoubleBiFunction;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("Hello ");
-        System.out.print("World");
-        System.out.println("Nia Watson");
-        System.out.print("Nia ");
-        System.out.print("Watson");
+//        System.out.println("Hello World");
+//        System.out.print("Hello ");
+//        System.out.print("World");
+//        System.out.println("Nia Watson");
+//        System.out.print("Nia ");
+//        System.out.print("Watson");
+
+        int myFavoriteNumber = 32;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string";
+        System.out.println(myString);
+
+        double myNumber = 3.4E38;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
 
