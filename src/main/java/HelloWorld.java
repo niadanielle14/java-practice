6b337816417d9499769b2d29fc1119f47fea8709
Nia Watson <nia.d.watson@gmail.com>
2022-09-15 18:46:34 -0500

    Time Converter

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.function.ToDoubleBiFunction;
 
 public class HelloWorld {
@@ -10,22 +11,40 @@ public class HelloWorld {
 //        System.out.print("Nia ");
 //        System.out.print("Watson");
 
-        int myFavoriteNumber = 32;
-        System.out.println(myFavoriteNumber);
-
-        String myString = "This is my string";
-        System.out.println(myString);
-
-        double myNumber = 3.4E38;
-        System.out.println(myNumber);
+//        int myFavoriteNumber = 32;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "This is my string";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.4E38;
+//        System.out.println(myNumber);
 
 //        int x = 5;
 //        System.out.println(x++);
 //        System.out.println(x);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//       TODO Time Converter
+//        You need a program to convert days to seconds.
+//        The given code takes the amount of days as input.
+//        Complete the code to convert it to seconds and output the result.
+
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+
+        // 1. figure out how many seconds are in a days - 86400
+        // 2. multiply the number of seconds in a day by the # of days
+        int result = days * 86400;
+
+        // 3. sout the result
+        System.out.println(result);
+
+//        TODO CONDITIONALS
+
     }
 }
 
