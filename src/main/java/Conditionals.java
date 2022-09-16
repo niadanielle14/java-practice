a65248f0d689809b8e10b45a90a92c3a0084f31a
Nia Watson <nia.d.watson@gmail.com>
2022-09-15 21:06:23 -0500

    Nested If Statements

--- src/main/java/Conditionals.java
+++ src/main/java/Conditionals.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.ToDoubleBiFunction;
 
 public class Conditionals {
     public static void main(String[] args) {
-        System.out.println("If Statements");
+        System.out.println("If Else Statements");
 //        TODO
 //          You have $12,000 to buy a car.
 //          You're given a program which takes the price of car as an input.
@@ -18,6 +20,27 @@ public class Conditionals {
 
         if (price <= 12000) {
             System.out.println("yes");
+        } else {
+            System.out.println("no");
         }
+        System.out.println("Nested IF Else");
+
+//        Todo
+//          The sales manager decided to give a gift card to the customers whose purchases total more than 15000. On top of this, the customers whose total purchase is above 30000 will receive a second gift card.
+//          You are given a program, which takes the purchase amount as input, and print "Gift card" if it is above 15000.
+//          Task
+//          Complete the code to print "Gift card" again if the purchase is above 30000.
+        Scanner read = new Scanner(System.in);
+        int purchases = read.nextInt();
+
+        if (purchases > 15000) {
+            System.out.println("Gift card");
+            //complete the code
+        }
+        if (purchases > 30000) {
+            System.out.println("Gift card");
+        }
+
     }
+
 }
